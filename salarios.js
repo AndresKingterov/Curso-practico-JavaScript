8f4cc24bb03a5dee2c3b14222e076ffadc5d609d
AndresKingterov <tec_grone56@hotmail.com>
2021-12-09 22:40:24 -0500

    tercer commit

---
+++ salarios.js
@@ -0,0 +1,72 @@
+const colombia =[];
+colombia.push(
+    {
+        name:"Andres", 
+        Salary:1000,
+
+    }
+);
+colombia.push(
+    {
+        name:"andio", 
+        Salary:1200,
+
+    }
+);
+colombia.push(
+    {
+        name:"Nath", 
+        Salary:600,
+
+    }
+);
+colombia.push(
+    {
+        name:"Juan", 
+        Salary:100,
+
+    }
+);
+colombia.push(
+    {
+        name:"Diego", 
+        Salary:5000,
+
+    }
+);
+
+colombia.push(
+    {
+        name:"Roman", 
+        Salary:600,
+
+    }
+);
+colombia.push(
+    {
+        name:"Remulo", 
+        Salary:900,
+
+    }
+);
+colombia.push(
+    {
+        name:"Jorda", 
+        Salary:950,
+
+    }
+);
+colombia.push(
+    {
+        name:"Ramiro", 
+        Salary:950,
+
+    }
+);
+colombia.push(
+    {
+        name:"Roncancio", 
+        Salary:950,
+
+    }
+);
